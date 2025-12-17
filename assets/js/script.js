ebb9eb17f9225a4502413f2aaacbe5d12e99b8df
mrosevere <mark.rosevere@gmail.com>
2025-12-17 17:37:47 +0000

    added the division back in

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,11 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division" ) {
+        displayDivisionQuestion(num1, num2);
+    } 
+    
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -89,7 +93,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "/") {
+        return [Math.floor(operand1 / operand2), "division"];
+    }
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -117,25 +124,31 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
+    document.getElementById('rounding-prompt').classList.remove("show"); // hide
+
     
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
-
+    document.getElementById('rounding-prompt').classList.remove("show"); // hide
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
-
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+    document.getElementById('rounding-prompt').classList.remove("show"); // hide
+}
 
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
+    document.getElementById('rounding-prompt').classList.add("show");   // show
 }
