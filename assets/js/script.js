a3c7e61455688ffcefb4d92ebec97fdf28709a97
mrosevere <mark.rosevere@gmail.com>
2025-12-17 15:19:02 +0000

    Add Addition Question

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,26 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+    runGame("gameType");
 })
 
 /**
  * The main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
 
     // Creates 2 random numbers between 1 and 25
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1,num2);
+    }
+    else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -44,7 +53,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAddionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
